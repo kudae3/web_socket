fd26debb8970d54f73a24b022f6dfc02d78d6aa1
Kudae Sithu <kudaesithu2@gmail.com>
2025-04-30 12:08:00 +0630

    socket-connection-setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import path from 'path';
+import { Server } from 'socket.io';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,11 +11,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (res, req) => {
-    req.sendFile(__dirname + "/public/index.html");
-  });
+  req.sendFile(__dirname + "/public/index.html");
+});
 
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Open http://localhost:${PORT} in your browser`);
-}
-);
+});
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(`New client connected: ${socket.id}`);
+});
